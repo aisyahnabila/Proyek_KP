07238a60ea8e5994f68308c8dd88a4fa178164fc
aisyahnabila <aisyahnabila2.an@gmail.com>
2024-07-09 11:29:28 +0700

    + style remember me ditambahkan

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -27,14 +27,14 @@
                     class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
                 <input type="email" id="email"
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="Masukkan Username Anda" required />
+                    placeholder="Masukkan Username" required />
             </div>
             <div class="mb-5">
                 <label for="password"
                     class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
                 <input type="password" id="password"
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    required />
+                    placeholder="Masukkan Password" required />
             </div>
             <div class="flex items-start mb-5">
                 <div class="flex items-center h-5">

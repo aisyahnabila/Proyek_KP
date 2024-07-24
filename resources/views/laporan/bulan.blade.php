246997a3b0cf0bc34b0a8f0a988da9399bfbb8c0
aisyahnabila <aisyahnabila2.an@gmail.com>
2024-07-24 10:14:39 +0700

    fix bug, fitur bulanan. Note: kurang fitur export excel di fitur bulanan

--- resources/views/laporan/bulan.blade.php
+++ resources/views/laporan/bulan.blade.php
@@ -52,7 +52,7 @@
             <table class="w-full text-sm text-left rtl:text-right text-gray-900 dark:text-gray-400">
                 <thead class="text-xs text-gray-700 uppercase bg-gray-300 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
-                        <th scope="col" class="px-5 py-3">Bulan</th>
+                        <th scope="col" class="px-5 py-3">Bulan Pengajuan</th>
                         <th scope="col" class="px-5 py-3">Unit Kerja</th>
                         <th scope="col" class="px-5 py-3">Kode Barang</th>
                         <th scope="col" class="px-5 py-3">Nama Barang</th>

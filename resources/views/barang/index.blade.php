106196dcb383307eacf215a5e5c5ff2020d973d0
aisyahnabila <aisyahnabila2.an@gmail.com>
2024-07-30 08:14:57 +0700

    perbaikan problems confllic css dan tailwind element di page barang/index.blade.php dan permintaan/create.blade.php

--- resources/views/barang/index.blade.php
+++ resources/views/barang/index.blade.php
@@ -27,7 +27,7 @@
                     @csrf
                     <input type="file" name="file" id="file-input" style="display: none;">
                     <button type="button" id="import-button"
-                        class="flex items-center justify-center focus:outline-none text-white font-semibold bg-green-600 hover:bg-green-700 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:focus:ring-green-900">
+                        class="flex items-center justify-center focus:outline-none text-white bg-green-600 hover:bg-green-700 focus:ring-4 focus:ring-green-300 rounded-lg text-sm px-5 py-2.5 dark:focus:ring-green-900">
                         <i class="fa-solid fa-upload mr-1"></i>
                         <span>Import</span>
                     </button>
@@ -37,7 +37,7 @@
             {{-- fitur tambah --}}
             <div>
                 <a href="{{ route('kelola.create') }}"
-                    class="flex items-center justify-center focus:outline-none text-white font-semibold bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:focus:ring-yellow-900">
+                    class="flex items-center justify-center focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 rounded-lg text-sm px-5 py-2.5 dark:focus:ring-yellow-900">
                     <i class="fa-solid fa-plus mr-1"></i>
                     Tambah
                 </a>

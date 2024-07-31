bb5521cd7e544d4140d6e19dcf6c82b262149390
rzqthya <athiyya.fan178@gmail.com>
2024-07-31 15:32:15 +0700

    +unit kerja

--- database/seeders/UnitKerjaSeeder.php
+++ database/seeders/UnitKerjaSeeder.php
@@ -14,11 +14,21 @@ class UnitKerjaSeeder extends Seeder
     public function run(): void
     {
         DB::table('unit_kerja')->insert([
-            ['nama_unit_kerja' => 'Divisi IT', 'created_at' => now(), 'updated_at' => now()],
-            ['nama_unit_kerja' => 'Divisi HRD', 'created_at' => now(), 'updated_at' => now()],
-            ['nama_unit_kerja' => 'Divisi Keuangan', 'created_at' => now(), 'updated_at' => now()],
-            ['nama_unit_kerja' => 'Divisi Pemasaran', 'created_at' => now(), 'updated_at' => now()],
-            ['nama_unit_kerja' => 'Divisi Produksi', 'created_at' => now(), 'updated_at' => now()],
+            ['nama_unit_kerja' => 'Sub Bagian Penyusunan Program dan Anggaran', 'created_at' => now(), 'updated_at' => now()],
+            ['nama_unit_kerja' => 'Sub Bagian Keuangan', 'created_at' => now(), 'updated_at' => now()],
+            ['nama_unit_kerja' => 'Sub Bagian Umum dan Kepegawaian', 'created_at' => now(), 'updated_at' => now()],
+            ['nama_unit_kerja' => 'Seksi Pengelolaan Data Fakir Miskin', 'created_at' => now(), 'updated_at' => now()],
+            ['nama_unit_kerja' => 'Seksi Pendataan Fakir Miskin', 'created_at' => now(), 'updated_at' => now()],
+            ['nama_unit_kerja' => 'Seksi Penyelenggaran & Perlindungan Jaminan Sosial', 'created_at' => now(), 'updated_at' => now()],
+            ['nama_unit_kerja' => 'Seksi Bencana Alam', 'created_at' => now(), 'updated_at' => now()],
+            ['nama_unit_kerja' => 'Seksi Bencana Sosial', 'created_at' => now(), 'updated_at' => now()],
+            ['nama_unit_kerja' => 'Seksi Penanganan Khusus Bagi Kelompok Rentan dan Layanan Dukungan Psikososial', 'created_at' => now(), 'updated_at' => now()],
+            ['nama_unit_kerja' => 'Seksi Kepahlawanan, Keperintisan dan Kesetiakawanan Sosial', 'created_at' => now(), 'updated_at' => now()],
+            ['nama_unit_kerja' => 'Seksi Partisipasi Sosial Masyarakat, Pengelolaan Sumber Dana Kesejahteraan Sosial', 'created_at' => now(), 'updated_at' => now()],
+            ['nama_unit_kerja' => 'Seksi Kewirausahaan Sosial dan Penyuluhan Sosial', 'created_at' => now(), 'updated_at' => now()],
+            ['nama_unit_kerja' => 'Seksi Rehabilitasi Sosial Tuna Sosial', 'created_at' => now(), 'updated_at' => now()],
+            ['nama_unit_kerja' => 'Seksi Rehabilitasi Sosial Disabilitas', 'created_at' => now(), 'updated_at' => now()],
+            ['nama_unit_kerja' => 'Seksi Rehabilitasi Sosial Anak dan Lanjut Usia', 'created_at' => now(), 'updated_at' => now()],
         ]);
     }
 }
--- resources/views/laporan/bulan.blade.php
+++ resources/views/laporan/bulan.blade.php
@@ -54,7 +54,7 @@
                 <input type="hidden" name="tahun" value="{{ request('tahun') }}">
                 <button type="submit"
                     class="flex text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    <i class="fa-regular fa-file-word mt-1 mr-2"></i> Export to Word
+                    <i class="fa-regular fa-file-word mt-1 mr-2"></i> Export
                 </button>
             </form>
         </div>

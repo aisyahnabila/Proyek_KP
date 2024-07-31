4eff8407928183fbe054c9ec572a3bf04a890c02
aisyahnabila <aisyahnabila2.an@gmail.com>
2024-07-31 09:51:34 +0700

    delete flash message lama

--- resources/views/permintaan/create.blade.php
+++ resources/views/permintaan/create.blade.php
@@ -81,22 +81,6 @@
                         @enderror
                     </div>
 
-                    {{-- Flash Messages --}}
-                    {{-- @if ($errors->any())
-                        <div class="mb-4 mt-4">
-                            <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
-                                role="alert">
-                                <strong class="font-bold">Error!</strong>
-                                <span class="block sm:inline">Ada beberapa masalah dengan input Anda.</span>
-                                <ul class="mt-2 list-disc list-inside text-sm text-red-600">
-                                    @foreach ($errors->all() as $error)
-                                        <li>{{ $error }}</li>
-                                    @endforeach
-                                </ul>
-                            </div>
-                        </div>
-                    @endif --}}
-
                     @if (session('success'))
                         <div class="mb-4">
                             <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative"

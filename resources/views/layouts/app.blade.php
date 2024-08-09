cfb762613536f2d626fd8b96e7192656bd05ede4
aisyahnabila <aisyahnabila2.an@gmail.com>
2024-08-09 11:56:24 +0700

    unable vite di layouts/app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>Careventory's Web</title>
-    @vite('resources/css/app.css')
+    {{-- @vite('resources/css/app.css') --}}
     {{-- UI and Font --}}
     <link href="https://cdn.jsdelivr.net/npm/flowbite@2.4.1/dist/flowbite.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
